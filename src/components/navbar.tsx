adeabffe4ff009793c7ce833551e24abe4ac22f0
Lenar Akhmadeev <lenarakhmadeev@gmail.com>
2024-07-14 00:33:48 +0300

    nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -33,8 +33,7 @@ import "@twa-dev/sdk";
 import { TonConnectButton } from "@tonconnect/ui-react";
 
 export const Navbar = () => {
-    const { network } = useTonConnect();
-
+  const { network } = useTonConnect();
 
   const searchInput = (
     <Input
@@ -59,37 +58,38 @@ export const Navbar = () => {
 
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-        <NavbarBrand className="gap-3 max-w-fit">
-          <Link
-            className="flex justify-start items-center gap-1"
-            color="foreground"
-            href="/"
-          >
-            <Logo />
-            <p className="font-bold text-inherit">ACME</p>
-          </Link>
-        </NavbarBrand>
-        <div className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <Link
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium",
-                )}
-                color="foreground"
-                href={item.href}
-              >
-                {item.label}
-              </Link>
-            </NavbarItem>
-          ))}
-        </div>
-      </NavbarContent>
+      {/*<NavbarContent className="basis-1/5 sm:basis-full" justify="start">*/}
+      {/*  <NavbarBrand className="gap-3 max-w-fit">*/}
+      {/*    <Link*/}
+      {/*      className="flex justify-start items-center gap-1"*/}
+      {/*      color="foreground"*/}
+      {/*      href="/"*/}
+      {/*    >*/}
+      {/*      <Logo />*/}
+      {/*      <p className="font-bold text-inherit">ACME</p>*/}
+      {/*    </Link>*/}
+      {/*  </NavbarBrand>*/}
+
+      {/*  /!*<div className="hidden lg:flex gap-4 justify-start ml-2">*!/*/}
+      {/*  /!*  {siteConfig.navItems.map((item) => (*!/*/}
+      {/*  /!*    <NavbarItem key={item.href}>*!/*/}
+      {/*  /!*      <Link*!/*/}
+      {/*  /!*        className={clsx(*!/*/}
+      {/*  /!*          linkStyles({ color: "foreground" }),*!/*/}
+      {/*  /!*          "data-[active=true]:text-primary data-[active=true]:font-medium",*!/*/}
+      {/*  /!*        )}*!/*/}
+      {/*  /!*        color="foreground"*!/*/}
+      {/*  /!*        href={item.href}*!/*/}
+      {/*  /!*      >*!/*/}
+      {/*  /!*        {item.label}*!/*/}
+      {/*  /!*      </Link>*!/*/}
+      {/*  /!*    </NavbarItem>*!/*/}
+      {/*  /!*  ))}*!/*/}
+      {/*  /!*</div>*!/*/}
+      {/*</NavbarContent>*/}
 
       <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
+        // className="hidden sm:flex basis-1/5 sm:basis-full"
         justify="end"
       >
         {/*<NavbarItem className="hidden sm:flex gap-2">*/}
@@ -105,32 +105,29 @@ export const Navbar = () => {
         {/*  <ThemeSwitch />*/}
         {/*</NavbarItem>*/}
         {/*<NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>*/}
-        <NavbarItem className="hidden md:flex">
+        <NavbarItem className="flex">
             <TonConnectButton />
-            <div>
-                {network
-                    ? network === CHAIN.MAINNET
-                        ? "mainnet"
-                        : "testnet"
-                    : "N/A"}
-            </div>
+            {/*<div>*/}
+            {/*    {network*/}
+            {/*        ? network === CHAIN.MAINNET*/}
+            {/*            ? "mainnet"*/}
+            {/*            : "testnet"*/}
+            {/*        : "N/A"}*/}
+            {/*</div>*/}
 
 
 
-          <Button
-            isExternal
-            as={Link}
-            className="text-sm font-normal text-default-600 bg-default-100"
-            href={siteConfig.links.sponsor}
-            startContent={<HeartFilledIcon className="text-danger" />}
-            variant="flat"
-          >
-            Sponsor
-          </Button>
+          {/*<Button*/}
+          {/*  isExternal*/}
+          {/*  as={Link}*/}
+          {/*  className="text-sm font-normal text-default-600 bg-default-100"*/}
+          {/*  href={siteConfig.links.sponsor}*/}
+          {/*  startContent={<HeartFilledIcon className="text-danger" />}*/}
+          {/*  variant="flat"*/}
+          {/*>*/}
+          {/*  Sponsor*/}
+          {/*</Button>*/}
         </NavbarItem>
-      </NavbarContent>
-
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
         {/*<Link isExternal href={siteConfig.links.github}>*/}
         {/*  <GithubIcon className="text-default-500" />*/}
         {/*</Link>*/}
@@ -139,9 +136,9 @@ export const Navbar = () => {
       </NavbarContent>
 
       <NavbarMenu>
-        {searchInput}
+        {/*{searchInput}*/}
         <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
+          {siteConfig.navItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
               <Link
                 color={
@@ -151,7 +148,7 @@ export const Navbar = () => {
                       ? "danger"
                       : "foreground"
                 }
-                href="#"
+                href={item.href}
                 size="lg"
               >
                 {item.label}
