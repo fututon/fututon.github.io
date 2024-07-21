aba69ead51692f81997ed18804548fe9748f4b82
Lenar Akhmadeev <lenarakhmadeev@gmail.com>
2024-07-22 00:43:30 +0300

    wip card ui

--- src/components/RoundCard.tsx
+++ src/components/RoundCard.tsx
@@ -12,8 +12,6 @@ export default function RoundCard({ contractAddress }) {
   const predictRoundContract = usePredictRoundContract(contractAddress);
   const [bet, setBet] = useState(0);
 
-  console.log("predictRoundContract", predictRoundContract)
-
   const stateToStr = (state) => {
     if (state == 0) return 'Новый';
     if (state == 1) return 'Ставки принимаются';
@@ -23,110 +21,184 @@ export default function RoundCard({ contractAddress }) {
     return 'Неизвестный'
   }
 
-  const prizeSum = predictRoundContract.roundInfo ? fromNano(predictRoundContract.roundInfo[2] +predictRoundContract.roundInfo[3]) : '';
+  const prizeSum = predictRoundContract.roundInfo ? fromNano(predictRoundContract.roundInfo[2] + predictRoundContract.roundInfo[3]) : '';
   const stateStr = predictRoundContract.roundInfo ? stateToStr(predictRoundContract?.roundInfo[1]) : '';
 
-  return (
-    <Card
-      fullWidth={true}
-    >
-      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        <h4 className="font-bold text-large">Live</h4>
+  const renderLoading = () => {
+    return (
+      <Card
+        fullWidth={true}
+      >
+        Loading...
+      </Card>
+    )
+  }
 
-        <div>
-          <div>Address: {predictRoundContract.address}</div>
-          <div>State: {stateStr}</div>
-        </div>
-      </CardHeader>
-      <Divider/>
-      <CardBody className="overflow-visible py-2 flex flex-col items-center">
+  const renderNewRound = () => {
+    return (
+      <Card
+        fullWidth={true}
+      >
+        later
+      </Card>
+    )
+  }
 
-        <div>
-          UP {predictRoundContract.upSum ?? "Loading..."}
-        </div>
+  const renderStartedBetting = () => {
+    return (
+      <Card
+        fullWidth={true}
+      >
+        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+          <h4 className="font-bold text-large">Next</h4>
+        </CardHeader>
+        <Divider/>
+        <CardBody className="overflow-visible py-2 flex flex-col items-center">
+          <div>
+            UP {predictRoundContract.upSum ?? "Loading..."}
+          </div>
 
-        <div className="flex flex-col gap-1 p-2 border-2 rounded-xl w-full">
-          <p className="text-tiny uppercase font-bold">Prize pool: {prizeSum} TON</p>
+          <div className="flex flex-col gap-1 p-2 border-2 rounded-xl w-full">
+            <p className="text-tiny uppercase font-bold">Prize pool: {prizeSum} TON</p>
 
-          <Input type="number" label="TON" onValueChange={v => setBet(Number.parseFloat(v))} />
+            <Input type="number" label="TON" onValueChange={v => setBet(Number.parseFloat(v))} />
 
-          <Button
-            color="success"
-            isDisabled={!connected || bet <= 0}
-            onClick={() => {
-              predictRoundContract.sendPlaceUp(bet);
-            }}
-          >
-            UP
-          </Button>
+            <Button
+              color="success"
+              isDisabled={!connected || bet <= 0}
+              onClick={() => {
+                predictRoundContract.sendPlaceUp(bet);
+              }}
+            >
+              UP
+            </Button>
 
-          <Button
-            color="danger"
-            isDisabled={!connected || bet <= 0}
-            onClick={() => {
-              predictRoundContract.sendPlaceDown(bet);
-            }}
-          >
-            DOWN
-          </Button>
-        </div>
+            <Button
+              color="danger"
+              isDisabled={!connected || bet <= 0}
+              onClick={() => {
+                predictRoundContract.sendPlaceDown(bet);
+              }}
+            >
+              DOWN
+            </Button>
+          </div>
 
-        <div>
-          DOWN {predictRoundContract.downSum ?? "Loading..."}
-        </div>
+          <div>
+            DOWN {predictRoundContract.downSum ?? "Loading..."}
+          </div>
+        </CardBody>
+      </Card>
+    )
+  }
 
-      </CardBody>
+  const renderFinishedBetting = () => {
+    return (
+      <Card
+        fullWidth={true}
+      >
+        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+          <h4 className="font-bold text-large">Starting...</h4>
+        </CardHeader>
+        <Divider/>
+        <CardBody className="overflow-visible py-2 flex flex-col items-center">
 
-    </Card>
-  )
+          <div>
+            UP {predictRoundContract.upSum ?? "Loading..."}
+          </div>
 
+          <div className="flex flex-col gap-1 p-2 border-2 rounded-xl w-full">
+            <p className="text-tiny uppercase font-bold">Prize pool: {prizeSum} TON</p>
+          </div>
 
-  // return (
-  //   <div className="Container">
-  //     <h3>Counter</h3>
-  //
-  //     <div>
-  //       <b>Wallet balance</b>
-  //     </div>
-  //
-  //     <div>
-  //       <b>Address</b>
-  //       <span>{predictRoundContract.address}</span>
-  //     </div>
-  //
-  //     <div>
-  //       <b>Up Sum</b>
-  //       <div>{predictRoundContract.upSum ?? "Loading..."}</div>
-  //     </div>
-  //
-  //     <div>
-  //       <b>Down Sum</b>
-  //       <div>{predictRoundContract.downSum ?? "Loading..."}</div>
-  //     </div>
-  //
-  //     <Input type="number" label="TON" onValueChange={v => setBet(Number.parseFloat(v))} />
-  //
-  //     <Button
-  //       color="success"
-  //       disabled={!connected || bet <= 0}
-  //       className={`Button ${connected ? "Active" : "Disabled"}`}
-  //       onClick={() => {
-  //         predictRoundContract.sendPlaceUp(bet);
-  //       }}
-  //     >
-  //       UP
-  //     </Button>
-  //
-  //     <Button
-  //       color="danger"
-  //       disabled={!connected || bet <= 0}
-  //       className={`Button ${connected ? "Active" : "Disabled"}`}
-  //       onClick={() => {
-  //         predictRoundContract.sendPlaceDown(bet);
-  //       }}
-  //     >
-  //       DOWN
-  //     </Button>
-  //   </div>
-  // );
+          <div>
+            DOWN {predictRoundContract.downSum ?? "Loading..."}
+          </div>
+        </CardBody>
+      </Card>
+    )
+  }
+
+  const renderStartedRound = () => {
+    const startPrice = predictRoundContract.roundInfo[4];
+
+    return (
+      <Card
+        fullWidth={true}
+      >
+        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+          <h4 className="font-bold text-large">Live</h4>
+        </CardHeader>
+        <Divider/>
+        <CardBody className="overflow-visible py-2 flex flex-col items-center">
+          <div>
+            UP {predictRoundContract.upSum ?? "Loading..."}
+          </div>
+
+          <div className="flex flex-col gap-1 p-2 border-2 rounded-xl w-full">
+            <p className="text-tiny uppercase font-bold">Prize pool: {prizeSum} TON</p>
+
+            <p className="text-tiny uppercase font-bold">Locked price: {startPrice} TON</p>
+            <p className="text-tiny uppercase font-bold">Current price price: ?? TON</p>
+          </div>
+
+          <div>
+            DOWN {predictRoundContract.downSum ?? "Loading..."}
+          </div>
+        </CardBody>
+      </Card>
+    )
+  }
+
+  const renderFinishedRound = () => {
+    const startPrice = predictRoundContract.roundInfo[4];
+    const finishPrice = predictRoundContract.roundInfo[4];
+
+    return (
+      <Card
+        fullWidth={true}
+      >
+        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+          <h4 className="font-bold text-large">Live</h4>
+        </CardHeader>
+        <Divider/>
+        <CardBody className="overflow-visible py-2 flex flex-col items-center">
+          <div>
+            UP {predictRoundContract.upSum ?? "Loading..."}
+          </div>
+
+          <div className="flex flex-col gap-1 p-2 border-2 rounded-xl w-full">
+            <p className="text-tiny uppercase font-bold">Prize pool: {prizeSum} TON</p>
+
+            <p className="text-tiny uppercase font-bold">Locked price: {startPrice} TON</p>
+            <p className="text-tiny uppercase font-bold">Finish price price: {finishPrice} TON</p>
+
+            <Button
+              color="primary"
+              isDisabled={!connected}
+              onClick={() => {}}
+            >
+              Withdraw prize
+            </Button>
+          </div>
+
+          <div>
+            DOWN {predictRoundContract.downSum ?? "Loading..."}
+          </div>
+        </CardBody>
+      </Card>
+    )
+  }
+
+  if (predictRoundContract.roundInfo) {
+    const roundState = predictRoundContract.roundInfo[1];
+
+    if (roundState == 0) return renderNewRound();
+    if (roundState == 1) return renderStartedBetting();
+    if (roundState == 2) return renderFinishedBetting();
+    if (roundState == 3) return renderStartedRound();
+    if (roundState == 4) return renderFinishedRound();
+  }
+
+  return renderLoading();
 }
